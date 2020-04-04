adac2e7d49154f47d76e87973d1b6c18c8603802
rebeccasun <sun.ruoy@husky.neu.edu>
2020-04-04 00:18:15 -0700

    updated todolist

--- hw9/src/main/java/view/TodoList.java
+++ hw9/src/main/java/view/TodoList.java
@@ -2,9 +2,16 @@ package view;
 
 // Mainly deal with sorting and display.
 
+import controller.commandlineparser.CommandLine;
+import controller.commandlineparser.ICommandLine;
+import controller.commandlineparser.Option;
 import exceptions.IllegalTodoException;
+import exceptions.InvalidCSVFileException;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import model.IItem;
 import model.comparators.ComparatorFactory;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
@@ -14,56 +21,88 @@ import java.util.ArrayList;
 import model.Todo;
 import model.filter.FilterPlatform;
 import model.filter.FilterSettings;
+import model.reader.CSVReader;
+import model.reader.IReader;
 import model.writer.CSVWriter;
 import model.writer.IWriter;
 
 public class TodoList extends ItemList<Todo> {
-      Set<Integer> usedId;
-      String filepath;
+  String filepath;
+  int numOftodo;
+  static final int COLUMN = 6;
+  static final String TRIM_REGEX = "^\"|\"$";
+  static final String SPLIT_REGEX = "\",";
+  static final String EMPTY = "";
+  static final String HEADER = "“id”,”text”,”completed”,”due”,”priority”,”category”";
 
-      public TodoList(String filepath) {
-        super();
-        this.filepath = filepath;
-        this.usedId = new HashSet<>();
+  public TodoList(String filepath) throws IOException {
+    super();
+    this.filepath = filepath;
+    this.numOftodo = this.initialize();
+  }
+
+  public int initialize() throws IOException {
+    int count = 0;
+    try {
+      IReader reader = new CSVReader(this.filepath);
+      HashMap<String, String> keyValuePair = reader.readNextRow();
+      while (keyValuePair != null) {
+        this.add(keyValuePair.get("id"), keyValuePair.get("text"), keyValuePair.get("completed"),
+            keyValuePair.get("due"), keyValuePair.get("priority"), keyValuePair.get("category"));
+        count += 1;
+        keyValuePair = reader.readNextRow();
       }
+      reader.close();
+      return count;
+    } catch (Exception e) {
+      return 0;
+    }
+  }
 
+  private void add(String id, String text, String completed, String due, String priority, String category) {
+    Todo newItem = new Todo(Integer.parseInt(id),text,completed,due,priority,category);
+    super.appendItem(newItem);
+  }
+
+  //TODO:.
   //can i use builder pattern?
-  public void add(String text, String completed, String due, String priority, String category)
+  public void addTodo(String description)
       throws IOException {
-    int newId = this.itemArrayList.get(-1).getId() + 1;
-    while (this.usedId.contains(newId)) {
-      newId += 1;
+    int newId = this.numOftodo + 1;
+    String[] columns = description.split(SPLIT_REGEX);
+    if (columns.length != TodoList.COLUMN - 1) {
+      throw new InvalidCSVFileException();
     }
-    Todo newItem = new Todo(newId,text,completed,due,priority,category);
+    Todo newItem = new Todo(newId,trimQuotes(columns[0]),trimQuotes(columns[1]),trimQuotes(columns[2]),
+        trimQuotes(columns[3]),trimQuotes(columns[4]));
     this.appendItem(newItem);
-    IWriter writer = new CSVWriter(this.filepath);
-    writer.addLine(newItem.toString());
-    writer.closeFile();
   }
-  @Override
-  public void appendItem(Todo todo) {
-    if (!this.usedId.contains(todo.getId())) {
-      super.appendItem(todo);
-      this.usedId.add(todo.getId());
-    } else {
-      throw new IllegalTodoException("duplicate identification number found");
-    }
+
+  /**
+   * Trim the starting and ending quote of a String.
+   *
+   * @param s the provided string.
+   * @return the trimmed string.
+   */
+  private static String trimQuotes(String s) {
+    return s.replaceAll(TRIM_REGEX, EMPTY);
   }
+
 
   //how to change a specific line??
   public void completed(int id) throws FileNotFoundException {
     for (Todo todo : this.itemArrayList) {
       if (todo.getId() == id) {
         todo.setCompleted(true);
-        this.updateCSV(this.filepath) ;
         return;
       }
     }
   }
 
-  public void updateCSV(String filepath) throws FileNotFoundException {
-    try(BufferedWriter writer = new BufferedWriter(new FileWriter(filepath))) {
-      for (Todo todo: this.itemArrayList) {
+  public void updateCSV(String filepath) {
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath))) {
+      writer.write(HEADER);
+      for (Todo todo : super.itemArrayList) {
         writer.write(todo.toString());
       }
     } catch (IOException e) {
@@ -86,18 +125,18 @@ public class TodoList extends ItemList<Todo> {
   }
 
   @Override
-  public void filter(int status) {
-    if (status == 1) {
-      FilterSettings both = new FilterSettings.Builder().incompleteTodo().selectCategory("Work")
-          .build();
-      FilterPlatform platform = new FilterPlatform(both);
-      platform.filter(this.itemArrayList);
-    } else if (status == 2) {
+  public void filter() {
+    ICommandLine commandLine = new CommandLine();
+    if (commandLine.hasOption("--show-incomplete") && commandLine.hasOption("--show-category")) {
+      FilterSettings both = new FilterSettings.Builder().incompleteTodo().selectCategory(commandLine.getOptionValues("--show-category")).build();
+      FilterPlatform platform1 = new FilterPlatform(both);
+      platform1.filter(this.itemArrayList);
+    } else if (commandLine.hasOption("--show-incomplete")) {
       FilterSettings onlyOne = new FilterSettings.Builder().incompleteTodo().build();
       FilterPlatform platform1 = new FilterPlatform(onlyOne);
       platform1.filter(this.itemArrayList);
     } else {
-      FilterSettings only12 = new FilterSettings.Builder().selectCategory("Work").build();
+      FilterSettings only12 = new FilterSettings.Builder().selectCategory(commandLine.getOptionValues("--show-category")).build();
       FilterPlatform platform3 = new FilterPlatform(only12);
       platform3.filter(this.itemArrayList);
     }
