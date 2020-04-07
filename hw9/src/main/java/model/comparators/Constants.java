a665aa14cd700872aa224d3604d4ab0db52737ae
Bryan <xbryanpei@gmail.com>
2020-04-07 00:35:40 -0700

    Finished main2

--- hw9/src/main/java/model/comparators/Constants.java
+++
@@ -1,9 +0,0 @@
-package model.comparators;
-
-/**
- * Constants used for comparator
- */
-//public class Constants {
-//  public static final String DATE = "date";
-//  public static final String PRIORITY = "priority";
-//}
